f68ea684892881a1c857a3d04b56fe43ab7de660
Gustavo Almeida <gustavo.almei2@hotmail.com>
2026-03-18 00:52:33 -0300

    feat: create a new `Layout` component to provide a basic application structure with a sidebar.

--- frontend/src/components/layout/index.tsx
+++ frontend/src/components/layout/index.tsx
@@ -2,8 +2,8 @@ import { Sidebar } from './Sidebar'
 
 export function Layout({ element }: { element: React.ReactNode }) {
   return (
-    <div className='flex h-screen w-screen bg-color-bg-primary dark:bg-color-bg-secondary scrollbar-custom overflow-hidden'>
-      <div className='flex flex-1 m-2 gap-4 overflow-hidden'>
+    <div className='flex h-[100svh] w-screen bg-color-bg-primary dark:bg-color-bg-secondary scrollbar-custom overflow-hidden'>
+      <div className='flex flex-1 min-h-0 m-2 gap-4 overflow-hidden'>
         <Sidebar />
         <main className='flex-1 overflow-y-auto overflow-x-auto'>
           {element}
